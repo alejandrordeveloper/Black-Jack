ef21042b837059ce46717bac22f9192424853ebf
Alejandro Ramirez <alejandrocohete@gmail.com>
2025-06-09 11:33:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let dealerSum = 0;
+let yourSum = 0;
+
+let dealerAceCount = 0;
+let yourAceCount = 0; 
+
+let hidden;
+let deck;
+
+let canHit = true; //allows the player (you) to draw while yourSum <= 21
